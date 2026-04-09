714edb5eaa0664734a00eb803db4b7f3927482ce
Krn2706 <kathrine@asimba.dk>
2026-04-09 15:53:35 +0200

    Refactor nickname generation function

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // Arrays med navne-dele 
 const firstPart = [
-    "Skatte", "Fnuller", "Putte", "Guld", "Musse", "Snuske", "Mælke", "Jelly", "Cookie", "Buller", "Kramme", "Prutte", "Misse", "Stjerne", "Tosse", "Grine", "Puller", "Sveske", "Mulle", "Tulle", "Smukke", "Sukker", "Lækker", "Sol", "Prinsesse", "Elske", "Charme", "Honning", "Nusse", "Skum", "Mini", "Prinse", "Sommer", "Puslinge", "Nuller", "Trylle", "Muller", "Pjuske", "Plysser", "Tutte", "Smør", "Fjolle", "Glitter", "Snurre", "Bobbel", "Plys", "Bamse", "Glimmer", "Snuller", "Fluffe", "Lulle", "Nutte", "Pludre", "Kuller", "Muffin", "Tuller", "Marcipan", "Tumle", "Chaos", "Buller", "Bøvle", "Nutte", "Space", "Latter", "Finurlig", "Cutie", "Bølle"
+  "Skatte", "Fnuller", "Putte", "Guld", "Musse", "Snuske", "Mælke", "Jelly", "Cookie", "Buller", 
+  "Kramme", "Guld", "Prutte", "Misse", "Stjerne", "Tosse", "Grine", "Puller", "Sveske", "Mulle", "Tulle", "Smukke", "Sukker", "Lækker", "Sol", "Prinsesse", "Elske", "Charme", "Lækker", "Honning", "Nusse", "Skum", "Mini", "Prinse", "Sommer", "Puslinge", "Kramme", "Nuller", "Bølle", "Muller", "Pjuske", "Plysser", "Tutte", "Smør", "Fjolle", "Glitter", "Snurre", "Bobbel", "Plys", "Lækker", "Bamse", "Glimmer", "Snuller", "Tutte", "Fluffe-","Lulle", "Nutte-", "Cutie", "Tuller", "Marcipan", "Tumle", "Chaos", "Bøvle", "Snurre", "Latter" 
 ];
 
 const secondPart = [
-    "mus", "gøj", "tøs", "bamse", "-fis", "mis", "bøtte", "bean", "monster", "mås", "bjørn", "klump", "top", "dyr", "kat", "lys", "-tosse", "fyr", "dukke", "bisken", "trold", "sikke", "-futmælk", "-ling", "kage", "fidus", "-bar", "panda", "-muuh", "fugl", "boble", "blomst", "stjerne", "fisk", "gøjl", "stråle", "-my", "fnug", "sky", "fe", "ælling", "hvalp", "tumling", "-smølf", "perle", "pjusk", "drys", "-putling", "-puff", "-mulle", "-blomst", "-muller", "-buller", "-bølle", "-bøf", "trunte", "basse", "skat", "-min", "-muller", "form", "-skovl", "brød", "orkan", "kriger", "top", "frit", "butt", "bøvl", "patootie"
+  "mus", "gøj", "-tøs", "bamse", "-fis", "mis", "-bøtte", "bean", "monster", "mås", 
+  "bjørn", "klump", "top", "dyr", "kat", "lys", "tosse", "-fyr", "dukke", "bisken", "trold", "sikke", "-futmælk", "-ling", "kage", "-fidus", "-bar", "panda", "-muuh", "fugl", "boble", "blomst", "stjerne", "fisk", "gøjl", "stråle", "my", "-top", "fnug", "sky", "blomst", "fe", "ælling", "hvalp", "tumling", "-smølf", "perle", "pjusk", "drys", "putling", "-puff", "pattote", "brød", "kriger", "butt
 ];
 
 // Funktion til at generere et tilfældigt kælenavn
 function generateNickname() {
-    const randomFirst = firstPart[Math.floor(Math.random() * firstPart.length)];
-    const randomSecond = secondPart[Math.floor(Math.random() * secondPart.length)];
-    document.getElementById("nickname").innerText = randomFirst + randomSecond; // Sammenføje uden mellemrum
+  const randomFirst = firstPart[Math.floor(Math.random() * firstPart.length)];
+  const randomSecond = secondPart[Math.floor(Math.random() * secondPart.length)];
+  document.getElementById("nickname").innerText = randomFirst + randomSecond;  // Sammenføje uden mellemrum
 }
 
 // Håndter knap klik
